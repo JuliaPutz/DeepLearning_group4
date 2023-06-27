e2a179a56b508e08b7bfc72bb1eb1111f4d87c9c
Julia <jputz@flashnet.at>
2023-06-27 18:36:48 +0200

    add functions to create batch

--- A3/main.py
+++ A3/main.py
@@ -3,6 +3,8 @@ from ds import ImageSegmentationDataset
 
 from datasets import load_dataset
 from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation
+from torch.utils.data import DataLoader
+from functools import partial
 
 models = [{"type":"mask2former","trained_on":"ade","purpose":"semantic","variant":"tiny",
            "huggingface_id":"facebook/mask2former-swin-tiny-ade-semantic"}]
@@ -23,6 +25,27 @@ def make_dataset(name = "segments/sidewalk-semantic"):
     train_dataset = ImageSegmentationDataset(train_ds, transform=train_transform)
     test_dataset = ImageSegmentationDataset(test_ds, transform=test_transform)
     return train_dataset, test_dataset
+
+def collate_fn_custom(batch, processor):
+    inputs = list(zip(*batch))
+    images = inputs[0]
+    segmentation_maps = inputs[1]
+
+    batch = processor(
+        images,
+        segmentation_maps = segmentation_maps,
+        return_tensors = "pt"
+    )
+    batch["original_images"] = inputs[2]
+    batch["original_segmentation_maps"] = inputs[3]
+
+    return batch
+
+def create_batch(train_ds, test_ds, batch_size, processor):
+    train_data_loader = DataLoader(train_ds, batch_size = batch_size, shuffle=True, collate_fn=partial(collate_fn_custom, processor = processor))
+    test_data_loader = DataLoader(test_ds, batch_size = batch_size, shuffle=False, collate_fn=partial(collate_fn_custom, processor = processor))
+
+    return train_data_loader, test_data_loader
 
 if __name__ == "__main__":
     # load dataset. Caches, so doesn't have to be repeatedly redownloaded. 
