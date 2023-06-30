530473ea184f7571320d2bd10d54d01d355a2ca7
Julia <jputz@flashnet.at>
2023-06-30 12:58:53 +0200

    restructure according to submission

---
+++ A3/src/helpers.py
@@ -0,0 +1,112 @@
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from huggingface_hub import hf_hub_download
+import albumentations as A
+import json
+
+
+# define standard values as cornerpoints of image normalization
+ADE_MEAN = np.array([123.675, 116.280, 103.530]) / 255
+ADE_STD = np.array([58.395, 57.120, 57.375]) / 255
+
+# define image transforms for training
+# todo: describe what each step does
+train_transform = A.Compose([
+    A.LongestMaxSize(max_size=1333),    
+    A.RandomCrop(width=512, height=512),
+    A.HorizontalFlip(p=0.5),
+    #A.Normalize(mean=ADE_MEAN, std=ADE_STD),
+])
+
+# define image transforms for testing
+test_transform = A.Compose([
+    A.Resize(width=512, height=512),
+    #A.Normalize(mean=ADE_MEAN, std=ADE_STD),
+])
+
+# create a dictionary for easy conversion between semantic mask values and actual labels
+id2label = json.load(open(hf_hub_download("segments/sidewalk-semantic", "id2label.json", repo_type="dataset"), "r"))
+id2label = {int(k):v for k,v in id2label.items()}
+
+def color_palette():
+    """Color palette that maps each class to RGB values.
+    This one is actually taken from ADE20k.
+    """
+    return [[120, 120, 120], [180, 120, 120], [6, 230, 230], [80, 50, 50],
+            [4, 200, 3], [120, 120, 80], [140, 140, 140], [204, 5, 255],
+            [230, 230, 230], [4, 250, 7], [224, 5, 255], [235, 255, 7],
+            [150, 5, 61], [120, 120, 70], [8, 255, 51], [255, 6, 82],
+            [143, 255, 140], [204, 255, 4], [255, 51, 7], [204, 70, 3],
+            [0, 102, 200], [61, 230, 250], [255, 6, 51], [11, 102, 255],
+            [255, 7, 71], [255, 9, 224], [9, 7, 230], [220, 220, 220],
+            [255, 9, 92], [112, 9, 255], [8, 255, 214], [7, 255, 224],
+            [255, 184, 6], [10, 255, 71], [255, 41, 10], [7, 255, 255],
+            [224, 255, 8], [102, 8, 255], [255, 61, 6], [255, 194, 7],
+            [255, 122, 8], [0, 255, 20], [255, 8, 41], [255, 5, 153],
+            [6, 51, 255], [235, 12, 255], [160, 150, 20], [0, 163, 255],
+            [140, 140, 140], [250, 10, 15], [20, 255, 0], [31, 255, 0],
+            [255, 31, 0], [255, 224, 0], [153, 255, 0], [0, 0, 255],
+            [255, 71, 0], [0, 235, 255], [0, 173, 255], [31, 0, 255],
+            [11, 200, 200], [255, 82, 0], [0, 255, 245], [0, 61, 255],
+            [0, 255, 112], [0, 255, 133], [255, 0, 0], [255, 163, 0],
+            [255, 102, 0], [194, 255, 0], [0, 143, 255], [51, 255, 0],
+            [0, 82, 255], [0, 255, 41], [0, 255, 173], [10, 0, 255],
+            [173, 255, 0], [0, 255, 153], [255, 92, 0], [255, 0, 255],
+            [255, 0, 245], [255, 0, 102], [255, 173, 0], [255, 0, 20],
+            [255, 184, 184], [0, 31, 255], [0, 255, 61], [0, 71, 255],
+            [255, 0, 204], [0, 255, 194], [0, 255, 82], [0, 10, 255],
+            [0, 112, 255], [51, 0, 255], [0, 194, 255], [0, 122, 255],
+            [0, 255, 163], [255, 153, 0], [0, 255, 10], [255, 112, 0],
+            [143, 255, 0], [82, 0, 255], [163, 255, 0], [255, 235, 0],
+            [8, 184, 170], [133, 0, 255], [0, 255, 92], [184, 0, 255],
+            [255, 0, 31], [0, 184, 255], [0, 214, 255], [255, 0, 112],
+            [92, 255, 0], [0, 224, 255], [112, 224, 255], [70, 184, 160],
+            [163, 0, 255], [153, 0, 255], [71, 255, 0], [255, 0, 163],
+            [255, 204, 0], [255, 0, 143], [0, 255, 235], [133, 255, 0],
+            [255, 0, 235], [245, 0, 255], [255, 0, 122], [255, 245, 0],
+            [10, 190, 212], [214, 255, 0], [0, 204, 255], [20, 0, 255],
+            [255, 255, 0], [0, 153, 255], [0, 41, 255], [0, 255, 204],
+            [41, 0, 255], [41, 255, 0], [173, 0, 255], [0, 245, 255],
+            [71, 0, 255], [122, 0, 255], [0, 255, 184], [0, 92, 255],
+            [184, 255, 0], [0, 133, 255], [255, 214, 0], [25, 194, 194],
+            [102, 255, 0], [92, 0, 255]]
+
+
+
+def infer_img(processor, model, image):
+    # with a given processor and model, perform semantic segmentation on the given image
+    if not isinstance(image, Image.Image):
+        image = Image.fromarray(image)
+    inputs = processor(images=image, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # process the output to get the segmentation
+    seg = processor.post_process_semantic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
+    return seg
+
+def show_overlay(img, seg):
+    '''Show the image overlaid with the segmentation'''
+    color_seg = np.zeros((seg.shape[0], seg.shape[1], 3), dtype=np.uint8) # height, width, 3
+    palette = color_palette()
+    for label, color in enumerate(palette):
+        color_seg[seg == label, :] = color
+    # Convert to BGR
+    color_seg = color_seg[..., ::-1]
+    # Show image + mask
+    img = np.array(img) * 0.5 + color_seg * 0.5
+    img = img.astype(np.uint8)
+
+    plt.figure(figsize=(15, 10))
+    plt.imshow(img)
+    plt.show()
+
+def denormalize(image):
+    # undo the normalization to return to a regular image for viewing
+    unnormalized_image = (image * np.array(ADE_STD)[:, None, None]) + np.array(ADE_MEAN)[:, None, None]
+    unnormalized_image = (unnormalized_image * 255).astype(np.uint8)
+    unnormalized_image = np.moveaxis(unnormalized_image, 0, -1)
+    return Image.fromarray(unnormalized_image)
---
+++ A3/src/main.py
@@ -0,0 +1,308 @@
+from helpers import *
+from ds import ImageSegmentationDataset
+
+from datasets import load_dataset
+from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation, MaskFormerForInstanceSegmentation
+from torch.utils.data import DataLoader
+from functools import partial
+from tqdm.auto import tqdm
+import evaluate
+import time
+import pandas as pd
+
+models = [{"type":"mask2former","trained_on":"ade","purpose":"semantic","variant":"tiny",
+           "huggingface_id":"facebook/mask2former-swin-tiny-ade-semantic"}]
+
+def load_model(name: str, id2label = None, modelType = Mask2FormerForUniversalSegmentation):
+    """
+    load the specified model from huggingface
+
+    name: Path or name of the model
+    id2label: dict representing available label and its ids
+    modelType: used for defining the model
+    """
+    processor = AutoImageProcessor.from_pretrained(name)
+    if id2label is None:
+        # default model
+        model = modelType.from_pretrained(name)
+    else:
+        # replace classification head
+        model = modelType.from_pretrained(name, id2label = id2label, ignore_mismatched_sizes=True)
+    return processor, model
+
+
+##### DATASET #####
+
+def make_dataset(name: str):
+    """
+    split dataset into train and test
+    and return them as ImageSegmentationDataset
+
+    name: Path or name of the dataset
+    """
+    dataset = load_dataset(name)
+    dataset = dataset.shuffle(seed=1)
+    dataset = dataset["train"].train_test_split(test_size=0.2)
+    train_ds = dataset["train"]
+    test_ds = dataset["test"]
+
+    train_dataset = ImageSegmentationDataset(train_ds, transform=train_transform)
+    test_dataset = ImageSegmentationDataset(test_ds, transform=test_transform)
+    return train_dataset, test_dataset
+
+def get_label_map(repo_id: str):
+    """
+    return labels that can possibly be assigned to each pixel
+
+    repo_id: Path or name of the dataset
+    """
+    filename = "id2label.json"
+    id2label = json.load(open(hf_hub_download(repo_id, filename, repo_type="dataset"), "r"))
+    id2label = {int(k):v for k,v in id2label.items()}
+    return id2label
+
+def collate_fn_custom(batch, processor):
+    """
+    bring batches into a format the model expects
+    """
+    inputs = list(zip(*batch))
+    images = inputs[0]
+    segmentation_maps = inputs[1]
+
+    batch = processor(
+        images,
+        segmentation_maps = segmentation_maps,
+        return_tensors = "pt"
+    )
+    batch["original_images"] = inputs[2]
+    batch["original_segmentation_maps"] = inputs[3]
+
+    return batch
+
+def create_batch(train_ds: ImageSegmentationDataset, test_ds: ImageSegmentationDataset, batch_size: int, processor):
+    """
+    create batches for train and test set using DataLoader
+
+    train_ds: dataset from which to load the data for training
+    test_ds: dataset from which to load the data for training
+    batch_size: how many samples per batch to load
+    processor: processor to use for bringing data into the format the model expects
+    """
+    train_data_loader = DataLoader(train_ds, batch_size = batch_size, shuffle=True, collate_fn=partial(collate_fn_custom, processor = processor))
+    test_data_loader = DataLoader(test_ds, batch_size = batch_size, shuffle=False, collate_fn=partial(collate_fn_custom, processor = processor))
+
+    return train_data_loader, test_data_loader
+
+def print_initial_loss(model, batch):
+    """
+    prints loss of initial pre-trained model
+
+    model: model to compute loss for
+    """
+    outputs = model(batch["pixel_values"],
+                    class_labels = batch["class_labels"],
+                    mask_labels = batch["mask_labels"])
+    
+    print(f"Initial loss: {outputs.loss}")
+    return outputs
+
+
+##### TRAINING #####
+
+def train_model(model, train_data_loader: DataLoader, device, epochs: int = 2):
+    """
+    returns the model trained for specified number of epochs
+
+    model: model to fine-tune
+    train_data_loader: data to use for training
+    device: defines whether to use cuda or cpu
+    epochs: how many epochs to train
+    """
+    optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
+    running_loss = 0.0
+    num_samples = 0
+
+    for epoch in range(epochs):
+        print(f'Epoch: {epoch}')
+
+        #train
+        model.train()
+        for idx, batch in enumerate(tqdm(train_data_loader)):
+            optimizer.zero_grad() # reset parameter gradients
+
+            # forward pass
+            outputs  = model(pixel_values = batch["pixel_values"].to(device),
+                             mask_labels = [labels.to(device) for labels in batch["mask_labels"]],
+                             class_labels = [labels.to(device) for labels in batch["class_labels"]])
+            
+            # propagate backwards
+            loss = outputs.loss
+            loss.backward()
+
+            batch_size = batch["pixel_values"].size(0)
+            running_loss += loss.item()
+            num_samples += batch_size
+
+            if idx % 100 == 0:
+                print(f'\nLoss: {running_loss/num_samples}')
+
+            # optimize
+            optimizer.step()
+    return model
+
+
+##### EVALUATION #####
+
+def eval_model(model, test_data_loader: DataLoader, processor, device, num_batches: int = 0, show_times: bool = False):
+    """
+    returns the mean_iou of the passed model
+
+    model: model to evaluate
+    test_data_loader: data to use for evaluating the model
+    processor: used to perform semantic segmentation
+    device: defines whether to use cuda or cpu
+    num_batches: number of batches to use for evaluation; 0 if all batches should be used
+    show_times: if True print information about time needed for seperate steps
+    """
+
+    # evaluate
+    start_time = time.time()
+    metric = evaluate.load("mean_iou")
+    model = model.to(device)
+    model.eval()
+    setting_time = time.time()
+    if show_times: print(f"model setup: {(setting_time - start_time)} seconds")
+    for idx, batch in enumerate(tqdm(test_data_loader)):
+        if num_batches > 0:
+            if idx > num_batches:
+                break
+        epoch_start_time = time.time()
+        pixel_values = batch["pixel_values"]
+
+        # forward pass
+        with torch.no_grad():
+            outputs = model(pixel_values = pixel_values.to(device))
+        forward_pass_time = time.time()
+        if show_times: print(f"forward pass time: {(forward_pass_time-epoch_start_time)} seconds")
+        # original images
+        original_images = batch["original_images"]
+        target_sizes = [(image.shape[0], image.shape[1]) for image in original_images]
+        loading_time = time.time()
+        if show_times: print(f"image loading time: {(loading_time - forward_pass_time)} seconds")
+        # compare predict + ground truth segmentation maps
+        predicted_segmentation_maps = processor.post_process_semantic_segmentation(outputs, target_sizes = target_sizes)
+        ground_truth_segmentation_maps = batch["original_segmentation_maps"]
+        processing_time = time.time()
+        if show_times: print(f"image processing time: {(processing_time-loading_time)} seconds")
+        metric.add_batch(references = torch.tensor(np.array(ground_truth_segmentation_maps)).to(device), predictions = torch.stack(predicted_segmentation_maps).to(device))
+        eval_time = time.time()
+        if show_times: print(f"evaluation time: {(eval_time-processing_time)} seconds")
+        
+    final_time = time.time()
+    print(f"total time: {(final_time-start_time)} seconds")
+
+    return metric
+    
+def iou(mask1, mask2):
+    mask1_area = torch.count_nonzero(mask1)
+    mask2_area = torch.count_nonzero(mask2)
+    intersection = torch.count_nonzero(torch.logical_and(mask1, mask2))
+    iou = intersection / (mask1_area + mask2_area - intersection)
+    return iou.item()
+
+def get_ious(pred, truth):
+    uniques = truth.unique()
+    ious = {}
+    for i in uniques:
+        ious[i.item()] = iou(pred == i, truth == i)
+    ious["mean"] = np.mean(list(ious.values()))
+    return ious
+
+def eval_model2(modelName, eval_dataloader, modelType = Mask2FormerForUniversalSegmentation):
+    """
+    custom evaluation method computing mean_iou
+
+    modelName: model to evaluate
+    eval_dataloader: data used to evaluate model
+    """
+    if isinstance(modelName, str):
+        model = modelType.from_pretrained(modelName).to(device)
+    else:
+        model = modelName
+    resultlist = []
+    for x in tqdm(eval_dataloader):
+        img = x["pixel_values"]
+        with torch.no_grad():
+            output = model(img.to(device))
+
+        original_image = x["original_images"][0]
+        target_size = (original_image.shape[0], original_image.shape[1])
+        pred = processor.post_process_semantic_segmentation(output, target_sizes = [target_size])[0]
+        truth = x["original_segmentation_maps"][0]
+        ious = get_ious(pred, torch.tensor(truth).to(device))
+        resultlist.append(ious)
+    ioudf = pd.DataFrame(resultlist)
+    ioudf = pd.concat([ioudf["mean"], ioudf.drop("mean", axis=1).sort_index(axis=1)], axis=1)
+    ioudf = ioudf.rename(columns=id2label)
+    return ioudf
+
+
+def inference(model, test_dataloader, processor):
+    """
+    perform prediction using new model
+    compare overlay for one image of ground truth with new prediction
+
+    model: model used to predict segmentation
+    test_dataloader: data to take example image from
+    processor: used to predict segmentation
+    """
+    batch = next(iter(test_dataloader))
+
+    # show new prediction for one image
+    image = batch["original_images"][0]
+    pred = infer_img(processor, model, image)
+    show_overlay(image, pred)
+
+    # ground truth
+    segmentation_map = batch["original_segmentation_maps"][0]
+    show_overlay(image, segmentation_map)
+
+
+
+
+if __name__ == "__main__":
+    # use cuda if it is available (recommended)
+    if torch.cuda.is_available():
+        print("using cuda!")
+        device = "cuda:0"
+    else:
+        print("using cpu.")
+        device = "cpu"
+    print(device)
+
+    # load dataset and label map
+    name = "segments/sidewalk-semantic"
+    train_ds, test_ds = make_dataset(name)
+    id2label = get_label_map(name)
+    print(id2label)
+
+    # by passing the custom label dictionary, the classification head is replaced!
+    processor, model = load_model(models[0]["huggingface_id"], id2label = id2label)
+
+    # creating batches
+    train_dataloader, test_dataloader = create_batch(train_ds, test_ds, 2, processor)
+
+    # checking initial performance...
+    metric = eval_model(model, test_dataloader, processor, device, num_batches=5)
+    print(f"Pre-Finetuning Mean IoU: {metric.compute(num_labels = len(id2label), ignore_index=0)['mean_iou']}")
+
+    # tuning! ~5 minutes per epoch...
+    n_epochs = 20
+    finetuned_model = train_model(model, train_dataloader, device, epochs = n_epochs)
+    finetuned_model.save_pretrained(f"mask2former_finetuned_{name.replace('/','-')}_{n_epochs}epochs")
+
+    # evaluation of finetuned model!
+    metric = eval_model(finetuned_model, test_dataloader, processor, device)
+    metric_custom = eval_model2(finetuned_model, test_dataloader)
+    print(f"Post-Finetuning Mean IoU: {metric.compute(num_labels = len(id2label), ignore_index=0)['mean_iou']}")
+    print(f'Post-Finetuning custom Mean IoU: {metric_custom["mean"].mean()}')
